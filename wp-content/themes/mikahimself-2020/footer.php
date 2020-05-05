f977e234384b42d5f8f28fe7345d36e4ecbafecb
Mika Laihanen <mika.laihanen@basware.com>
2020-05-05 22:41:38 +0300

    Add sticky footer and bootstrap links.

--- wp-content/themes/mikahimself-2020/footer.php
+++ wp-content/themes/mikahimself-2020/footer.php
@@ -11,8 +11,8 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+	<footer id="colophon" class="footer">
+		<div class="container">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mikahimself-2020' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
